2d98ac1feb609314568f3a9a4ef792c982aa421d
Mariusz I <mariusz.iwanski@smtsoftware.com>
2015-09-01 15:29:18 +0200

    SampleBot (MVC Aplication) added

---
+++ HelloWars/SampleBot/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleBot.Startup))]
+namespace SampleBot
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
