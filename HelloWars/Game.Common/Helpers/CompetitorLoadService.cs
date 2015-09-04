6d2ffe97e050526804ea8d599fa7c49bbf493d11
Mateusz Furman <mateusz.furman@smtsoftware.com>
2015-09-04 11:55:20 +0200

    code formatting

--- HelloWars/Game.Common/Helpers/CompetitorLoadService.cs
+++ HelloWars/Game.Common/Helpers/CompetitorLoadService.cs
@@ -10,13 +10,14 @@ namespace Common.Helpers
         public async Task<ICompetitor> LoadCompetitorAsync(string baseUrl, string gameType)
         {
             var competitor = await WebClientHelper.GetDataAsync<CompetitorInfo>(baseUrl + Resources.InfoUrlSuffix);
-            
+
             if (competitor.GameType != gameType)
+            {
                 return null;
+            }
 
             competitor.Url = baseUrl;
             competitor.Id = Guid.NewGuid();
-            
 
             return competitor;
         }
