2980d7336ecdb10c0c9c8183242b8f2ddf9a8489
Mariusz I <mariusz.iwanski@smtsoftware.com>
2015-10-01 15:35:02 +0200

    RoundRobin NotVerifiedBot error fix, ResetButton added, unused function deleted.

---
+++ Arena/src/Elimination.RoundRobin/Commands/ResetCommand.cs
@@ -0,0 +1,25 @@
+﻿using Common.Utilities;
+using Elimination.RoundRobin.ViewModels;
+
+namespace Elimination.RoundRobin.Commands
+{
+    public class ResetCommand : CommandBase
+    {
+        protected readonly RoundRobinViewModel _viewModel;
+
+        public ResetCommand(RoundRobinViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public override void Execute(object parameter = null)
+        {
+            foreach (var bot in _viewModel.Bots)
+            {
+                bot.Wins = 0;
+                bot.Loses = 0;
+                bot.PlayAgainstId.Clear();
+            }
+        }
+    }
+}
